2105236cdeb4b1c06f35873858d211d549e24d8f
Carenalga <mapedorr@gmail.com>
2019-10-28 13:57:44 -0500

    The camera follows the playable character

--- Characters/PC/PC.gd
+++ Characters/PC/PC.gd
@@ -1,15 +1,15 @@
 extends Actor
 
 func _physics_process(delta):
-	var is_jump_interrupted = Input.is_action_just_released("jump") and velocity.y < 0.0
+	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(
-		velocity,
+	_velocity = calculate_move_velocity(
+		_velocity,
 		direction,
 		speed,
 		is_jump_interrupted
 	)
-	velocity = move_and_slide(velocity, FLOOR_NORMAL)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
 func get_direction() -> Vector2:
 	return Vector2(
