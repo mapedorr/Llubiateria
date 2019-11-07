4df0c12b451980840cd2b85a4b9d1c5693edf283
Carenalga <mapedorr@gmail.com>
2019-11-07 17:08:20 -0500

    Adjustments on jump falling.
    The player can not jump until she releases the jump button.
    Added the a placeholder for the armor sprite.
    Moved the Point2D for the battery placement in the PC.

--- Characters/PC/PC.gd
+++ Characters/PC/PC.gd
@@ -5,6 +5,8 @@ var can_take_battery = false
 var battery_resource = load("res://Battery/Battery.tscn")
 var battery = null
 var object_taken = false
+var cooldown_time = 0.2
+var can_jump = true
 
 func _ready():
 	Events.connect("damage_inflicted", self, "damage_character")
@@ -21,9 +23,12 @@ func _physics_process(delta):
 	if Input.is_action_just_pressed("Fire"):
 		if can_take_battery == false:
 			throw_battery()
+	if not can_jump:
+		can_jump = Input.is_action_just_released("jump")
 	var is_jump_interrupted = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	$Health.value = health
 	var direction: = get_direction()
+	
 	_velocity = calculate_move_velocity(
 		_velocity,
 		direction,
@@ -47,10 +52,14 @@ func calculate_move_velocity(
 	var new_velocity = linear_velocity
 	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
-	if direction.y == -1.0:
+	if direction.y == -1.0 and can_jump:
+		can_jump = false
 		new_velocity.y = speed.y * direction.y
+	elif _velocity.y > 0.0:
+		new_velocity.y = 1300.0
 	if is_jump_interrupted:
-		new_velocity.y = 0.0
+		can_jump = true
+		new_velocity.y = 1300.0 + _velocity.y
 	return new_velocity
 
 func damage_character():
